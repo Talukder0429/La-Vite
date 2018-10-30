2c11aaa824a93ecc4fdece8c1eac67530fadcba0
oopsidoodles <vilimilner@gmail.com>
2018-10-30 00:35:14 -0400

    able to add params to querybuilder

--- project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
+++ project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
@@ -1,13 +1,28 @@
 package queryhelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class QueryBuilder
 {
-	private List<String> paramsList;
+	private List<QueryParameter> paramsList;
 	
-	public QueryBuilder()
+	private String tableName;
+	
+	
+	
+	public QueryBuilder(String tableName)
 	{
+		this.paramsList = new ArrayList<QueryParameter>();
 		
+		this.tableName = tableName;
+	}
+	
+	
+	
+	public void AddParam(String name, String value)
+	{
+		QueryParameter qp = new QueryParameter(name, value);
+		this.paramsList.add(qp);
 	}
 }

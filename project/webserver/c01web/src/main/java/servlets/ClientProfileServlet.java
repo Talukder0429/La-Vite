4cf35cb342a9afc6c7f8cafd9597eac8a1d56415
oopsidoodles <vilimilner@gmail.com>
2018-10-30 01:11:01 -0400

    clientprofile servlet now uses querybuilder with requesthelper

--- project/webserver/c01web/src/main/java/servlets/ClientProfileServlet.java
+++ project/webserver/c01web/src/main/java/servlets/ClientProfileServlet.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import queryhelper.FieldSpec;
+import queryhelper.HttpServletRequestHelper;
+import queryhelper.QueryBuilder;
+
 import java.sql.*;
 import java.util.Scanner;
 
@@ -83,55 +87,29 @@ public class ClientProfileServlet extends HttpServlet {
 						dbpass);
 				)
 		{
-			//TODO cleaning of fields should be in servlet, not in client
-			//Gets params from request
-			String unique_identifier = GetParam(request, "unique_identifier");
-			String unique_identifier_value = GetParam(request, "unique_identifier_value");
-			String date_of_birth = GetParam(request, "date_of_birth");
-			String phone_number = GetParam(request, "phone_number");
-			String has_email_address = GetParam(request, "has_email_address");
-			String email_address = GetParam(request, "email_address");
-			String street_number = GetParam(request, "street_number");
-			String street_name = GetParam(request, "street_name");
-			String street_type = GetParam(request, "street_type");
-			String street_direction = GetParam(request, "street_direction");
-			String unit = GetParam(request, "unit");
-			String city = GetParam(request, "city");
-			String province = GetParam(request, "province");
-			String postal_code = GetParam(request, "postal_code");
-			String language = GetParam(request, "language");
-			String has_consent = GetParam(request, "has_consent");
+			QueryBuilder qb = new QueryBuilder(FieldSpec.TABLE_CLIENT_PROFILE);
+			HttpServletRequestHelper helper = new HttpServletRequestHelper(request, qb);
 			
-			//Query string
-			String query = "INSERT INTO ";
-			query += "client_profile"; //TODO table name
-			query += "(unique_identifier, unique_identifier_value, date_of_birth, phone_number, has_email_address, "
-					+ "email_address, street_number, street_name, street_type, street_direction, "
-					+ "unit, city, province, postal_code, language, "
-					+ "has_consent) ";
-			query += "VALUES(?,?,?,?,?,"
-					+ "?,?,?,?,?,"
-					+ "?,?,?,?,?,"
-					+ "?)"; //Add extra question mark for each attribute
+			//for each new field, must insert it into the query builder
+			helper.addToQuery(FieldSpec.UNIQUE_IDENTIFIER);
+			helper.addToQuery(FieldSpec.UNIQUE_IDENTIFIER_VALUE);
+			helper.addToQuery(FieldSpec.DATE_OF_BIRTH);
+			helper.addToQuery(FieldSpec.PHONE_NUMBER);
+			helper.addToQuery(FieldSpec.HAS_EMAIL_ADDRESS);
+			helper.addToQuery(FieldSpec.EMAIL_ADDRESS);
+			helper.addToQuery(FieldSpec.STREET_NUMBER);
+			helper.addToQuery(FieldSpec.STREET_NAME);
+			helper.addToQuery(FieldSpec.STREET_TYPE);
+			helper.addToQuery(FieldSpec.STREET_DIRECTION);
+			helper.addToQuery(FieldSpec.UNIT);
+			helper.addToQuery(FieldSpec.CITY);
+			helper.addToQuery(FieldSpec.PROVINCE);
+			helper.addToQuery(FieldSpec.POSTAL_CODE);
+			helper.addToQuery(FieldSpec.LANGUAGE);
+			helper.addToQuery(FieldSpec.HAS_CONSENT);
 
 			//Sets values into query
-			PreparedStatement querySql = conn.prepareStatement(query);
-			querySql.setString(1, unique_identifier);
-			querySql.setString(2, unique_identifier_value);
-			querySql.setString(3, date_of_birth);
-			querySql.setString(4, phone_number);
-			querySql.setString(5, has_email_address);
-			querySql.setString(6, email_address);
-			querySql.setString(7, street_number);
-			querySql.setString(8, street_name);
-			querySql.setString(9, street_type);
-			querySql.setString(10, street_direction);
-			querySql.setString(11, unit);
-			querySql.setString(12, city);
-			querySql.setString(13, province);
-			querySql.setString(14, postal_code);
-			querySql.setString(15, language);
-			querySql.setString(16, has_consent);
+			PreparedStatement querySql = conn.prepareStatement(qb.generateQueryString());
 			querySql.executeUpdate();
 
 			response.setStatus(HttpServletResponse.SC_OK);
@@ -141,10 +119,4 @@ public class ClientProfileServlet extends HttpServlet {
 			response.getOutputStream().println(e.getMessage());
 		}
 	}
-	
-	private String GetParam(HttpServletRequest request, String param)
-	{
-		return request.getParameter(param);
-	}
-
 }

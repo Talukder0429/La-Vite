5af2cc070619d3db4c799fa0b5903df83bc0a7a2
oopsidoodles <vilimilner@gmail.com>
2018-10-30 00:43:55 -0400

    querybuilder can now build question marks string for prepared statement object

--- project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
+++ project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
@@ -52,4 +52,25 @@ public class QueryBuilder
 		
 		return result;
 	}
+	
+	private String generateQuestionMarks() //returns question marks for the prepared statement object
+	{
+		String result = "(";
+		
+		for (int a = 0; a < this.paramsList.size(); a++)
+		{
+			String add = "?";
+			
+			if (a != this.paramsList.size() - 1) //not last field
+			{
+				add += ",";
+			}
+			
+			result += add;
+		}
+		
+		result += ")";
+		
+		return result;
+	}
 }

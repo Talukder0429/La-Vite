d2cf565d0eb86d2b51906668adca16eae034dc16
oopsidoodles <vilimilner@gmail.com>
2018-10-30 00:09:40 -0400

    Added queryhelper package, with filespec class

---
+++ project/webserver/c01web/src/main/java/queryhelper/FieldSpec.java
@@ -0,0 +1,7 @@
+package queryhelper;
+
+public final class FieldSpec
+{
+	public static final String UNIQUE_IDENTIFIER = "unique_identifier";
+	public static final String UNIQUE_IDENTIFIER_VALUE = "unique_identifier_value";
+}

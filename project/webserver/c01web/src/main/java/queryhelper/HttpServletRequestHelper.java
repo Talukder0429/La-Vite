910aceda057f638c26ee60c46abeb5926248bbd7
oopsidoodles <vilimilner@gmail.com>
2018-10-30 01:07:23 -0400

    request helper is now able to add params to querybuilder by retrieving them too

--- project/webserver/c01web/src/main/java/queryhelper/HttpServletRequestHelper.java
+++ project/webserver/c01web/src/main/java/queryhelper/HttpServletRequestHelper.java
@@ -5,14 +5,21 @@ import javax.servlet.http.HttpServletRequest;
 public class HttpServletRequestHelper
 {
 	private HttpServletRequest request;
+	private QueryBuilder queryBuilder;
 	
-	public HttpServletRequestHelper(HttpServletRequest request)
+	public HttpServletRequestHelper(HttpServletRequest request, QueryBuilder queryBuilder)
 	{
 		this.request = request;
+		this.queryBuilder = queryBuilder;
 	}
 	
-	public String GetParam(String name)
+	public String getParam(String name)
 	{
 		return this.request.getParameter(name);
+	}
+	
+	public void addToQuery(String name)
+	{
+		this.queryBuilder.addParam(name, this.getParam(name));
 	}
 }

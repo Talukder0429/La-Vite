196e67c410c8c2d54e5789904b605376161ac95d
oopsidoodles <vilimilner@gmail.com>
2018-10-30 00:20:20 -0400

    removed type from builder, modified filespec to know field names as well as mappings, and be able to return them

--- project/webserver/c01web/src/main/java/queryhelper/FieldSpec.java
+++ project/webserver/c01web/src/main/java/queryhelper/FieldSpec.java
@@ -1,7 +1,29 @@
 package queryhelper;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public final class FieldSpec
 {
+	private static final Map<String, String> FIELD_TYPES = Collections.unmodifiableMap(
+		    new HashMap<String, String>() {
+			{
+				//Each new field must have a type associated to it
+		        put(UNIQUE_IDENTIFIER, FIELDTYPE_STRING);
+		        put(UNIQUE_IDENTIFIER_VALUE, FIELDTYPE_STRING);
+		    }});
+	
+	//Each new type goes here
+	public static final String FIELDTYPE_STRING = "string";
+	public static final String FIELDTYPE_BOOLEAN = "boolean";
+	
+	//Each new field has it's referenced name go here
 	public static final String UNIQUE_IDENTIFIER = "unique_identifier";
 	public static final String UNIQUE_IDENTIFIER_VALUE = "unique_identifier_value";
+	
+	public static final String GetType(String field)
+	{
+		return FIELD_TYPES.get(field);
+	}
 }
--- project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
+++ project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
@@ -1,10 +1,7 @@
 package queryhelper;
 
 public class QueryBuilder
-{
-	private static final String FIELDTYPE_STRING = "string";
-	private static final String FIELDTYPE_BOOLEAN = "boolean";
-	
+{	
 	public QueryBuilder()
 	{
 		

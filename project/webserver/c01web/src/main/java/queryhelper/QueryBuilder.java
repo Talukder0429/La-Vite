dafffd72a03ffffcda14a5c1361c3f097f8b4c94
oopsidoodles <vilimilner@gmail.com>
2018-10-30 00:14:32 -0400

    added QueryBuilder class

---
+++ project/webserver/c01web/src/main/java/queryhelper/QueryBuilder.java
@@ -0,0 +1,12 @@
+package queryhelper;
+
+public class QueryBuilder
+{
+	private static final String FIELDTYPE_STRING = "string";
+	private static final String FIELDTYPE_BOOLEAN = "boolean";
+	
+	public QueryBuilder()
+	{
+		
+	}
+}

5ddd4140c3bff38be785e37cb442f45f4ad4e2f2
Arnob <arnob_talukder@hotmail.com>
2018-11-07 01:12:30 -0500

    updated Fields and Lists js to include the first 103 inputs

--- project/webserver/c01web/WebContent/helpers/Fields.js
+++ project/webserver/c01web/WebContent/helpers/Fields.js
@@ -20,6 +20,19 @@ const UNIT = "unit";
 const CITY = "city";
 const POSTAL_CODE = "postal_code";
 
+	//103
+const PROCESSING_DETAILS = "processing_details";
+const UPDATE_RECORD_ID = "update_record_id";
+const UNIQUE_IDENTIFIER = "unique_identifier";
+const UNIQUE_IDENTIFIER_VALUE = "unique_identifier_value";
+const DATE_OF_BIRTH_YYYY_MM_DD = "date_of_birth_yyyy_mm_dd";
+const POSTAL_CODE_WHERE_THE_SERVICE_WAS_RECEIVED = "postal_code_where_the_service_was_received";
+const START_DATE_YYYY_MM_DD = "start_date_yyyy_mm_dd";
+const END_DATE_YYYY_MM_DD = "end_date_yyyy_mm_dd";
+const PROJECTED_END_DATE_YYYY_MM_DD = "projected_end_date_yyyy_mm_dd";
+const START_DATE_OF_SERVICE_YYYY_MM_DD = "start_date_of_service_yyyy_mm_dd";
+	//
+
 const END_DATE_OF_SERVICE_YYYY_MM_DD = "end_date_of_service_yyyy_mm_dd";
 const START_DATE_OF_ASSESSMENT_YYYY_MM_DD = "start_date_of_assessment_yyyy_mm_dd";
 const END_DATE_OF_ASSESSMENT_YYYY_MM_DD = "end_date_of_assessment_yyyy_mm_dd";
@@ -38,6 +51,69 @@ const TOTAL_NUMBER_OF_SPOTS_IN_COURSE = "total_number_of_spots_in_course";
 //Boolean fields go here
 const HAS_EMAIL_ADDRESS = "has_email_address";
 const HAS_CONSENT_FOR_FUTURE_RESEARCH_OR_CONSULTATION = "has_consent_for_future_research_or_consultation";
+
+	//103
+const DID_VOLUNTEERS_FROM_THE_HOST_COMMUNITY_PARTICIPATE_IN_THE_ACTIVITY = "did_volunteers_from_the_host_community_participate_in_the_activity";
+const DIRECTED_AT_A_SPECIFIC_TARGET_GROUP = "directed_at_a_specific_target_group";
+const TARGET_GROUP_CHILDREN_0_14_YRS = "target_group_children_0_14_yrs";
+const TARGET_GROUP_YOUTH_15_24_YRS = "target_group_youth_15_24_yrs";
+const TARGET_GROUP_SENIOR = "target_group_senior";
+const TARGET_GROUP_GENDER_SPECIFIC = "target_group_gender_specific";
+const TARGET_GROUP_REFUGEES = "target_group_refugees";
+const TARGET_GROUP_ETHNIC_CULTURAL_LINGUISTIC_GROUP = "target_group_ethnic_cultural_linguistic_group";
+const TARGET_GROUP_DEAF_OR_HARD_OF_HEARING = "target_group_deaf_or_hard_of_hearing";
+const TARGET_GROUP_BLIND_OR_PARTIALLY_SIGHTED = "target_group_blind_or_partially_sighted";
+const TARGET_GROUP_LESBIAN_GAY_BISEXUAL_TRANSGENDER_QUEER_LGBTQ = "target_group_lesbian_gay_bisexual_transgender_queer_lgbtq";
+const TARGET_GROUP_FAMILIES_PARENTS = "target_group_families_parents";
+const TARGET_GROUP_OTHER_IMPAIRMENTS_PHYSICAL_MENTAL = "target_group_other_impairments_physical_mental";
+const TARGET_GROUP_CLIENTS_WITH_INTERNATIONAL_TRAINING_IN_A_REGULATED_PROFESSION = "target_group_clients_with_international_training_in_a_regulated_profession";
+const TARGET_GROUP_CLIENTS_WITH_INTERNATIONAL_TRAINING_IN_A_REGULATED_TRADE = "target_group_clients_with_international_training_in_a_regulated_trade";
+const TARGET_GROUP_OFFICIAL_LANGUAGE_MINORITIES = "target_group_official_language_minorities";
+const WAS_ESSENTIAL_SKILLS_AND_APTITUDES_TRAINING_RECEIVED_AS_PART_OF_THE_SERVICE = "was_essential_skills_and_aptitudes_training_received_as_part_of_the_service";
+const COMPUTER_SKILLS = "computer_skills";
+const DOCUMENT_USE = "document_use";
+const INTERPERSONAL_SKILLS_AND_WORKPLACE_CULTURE = "interpersonal_skills_and_workplace_culture";
+const LEADERSHIP_TRAINING = "leadership_training";
+const LIFE_SKILLS = "life_skills";
+const NUMERACY = "numeracy";
+const SUPPORT_SERVICES_RECEIVED = "support_services_received";
+const CARE_FOR_NEWCOMER_CHILDREN = "care_for_newcomer_children";
+const TRANSPORTATION_CHILD = "transportation_child";
+const PROVISIONS_FOR_DISABILITIES = "provisions_for_disabilities";
+const TRANSLATION = "translation";
+const INTERPRETATION = "interpretation";
+const CRISIS_COUNSELLING = "crisis_counselling";
+const OVERVIEW_OF_CANADA = "overview_of_canada";
+const OVERVIEW_OF_CANADA_REFERRALS = "overview_of_canada_referrals";
+const SOURCES_OF_INFORMATION = "sources_of_information";
+const SOURCES_OF_INFORMATION_REFERRALS = "sources_of_information_referrals";
+const RIGHTS_AND_FREEDOMS = "rights_and_freedoms";
+const RIGHTS_AND_FREEDOMS_REFERRALS = "rights_and_freedoms_referrals";
+const CANADIAN_LAW_AND_JUSTICE = "canadian_law_and_justice";
+const CANADIAN_LAW_AND_JUSTICE_REFERRALS = "canadian_law_and_justice_referrals";
+const IMPORTANT_DOCUMENTS = "important_documents";
+const IMPORTANT_DOCUMENTS_REFERRALS = "important_documents_referrals";
+const IMPROVING_ENGLISH_OR_FRENCH = "improving_english_or_french";
+const IMPROVING_ENGLISH_OR_FRENCH_REFERRALS = "improving_english_or_french_referrals";
+const EMPLOYMENT_AND_INCOME = "employment_and_income";
+const EMPLOYMENT_AND_INCOME_REFERRALS = "employment_and_income_referrals";
+const EDUCATION = "education";
+const EDUCATION_REFERRALS = "education_referrals";
+const HOUSING = "housing";
+const HOUSING_REFERRALS = "housing_referrals";
+const HEALTH = "health";
+const HEALTH_REFERRALS = "health_referrals";
+const MONEY_AND_FINANCES = "money_and_finances";
+const MONEY_AND_FINANCES_REFERRALS = "money_and_finances_referrals";
+const TRANSPORTATION_ADULT = "transportation_adult";
+const TRANSPORTATION_REFERRALS = "transportation_referrals";
+const COMMUNICATIONS_AND_MEDIA = "communications_and_media";
+const COMMUNICATIONS_AND_MEDIA_REFERRALS = "communications_and_media_referrals";
+const COMMUNITY_ENGAGEMENT = "community_engagement";
+const COMMUNITY_ENGAGEMENT_REFERRALS = "community_engagement_referrals";
+const BECOMING_A_CANADIAN_CITIZEN = "becoming_a_canadian_citizen";
+const BECOMING_A_CANADIAN_CITIZEN_REFERRALS = "becoming_a_canadian_citizen_referrals";
+	//
 
 const INTERPERSONAL_CONFLICT = "interpersonal_conflict";
 const INTERPERSONAL_CONFLICT_REFERRALS = "interpersonal_conflict_referrals";
@@ -127,6 +203,42 @@ const TIME_SPENT_WITH_CLIENT_ADDRESSING_CLIENTS_EMPLOYMENT_NEEDS_MINUTES = "time
 const FORMAT_OF_TRAINING_PROVIDED = "format_of_training_provided";
 const CLASSES_HELD_AT = "classes_held_at";
 
+//103
+const LANGUAGE_OF_SERVICE = "language_of_service";
+const OFFICIAL_LANGUAGE_OF_PREFERENCE = "official_language_of_preference";
+const REFERRED_BY = "referred_by";
+const ACTIVITY_UNDER_WHICH_CLIENT_RECEIVED_SERVICES = "activity_under_which_client_received_services";
+const TYPE_OF_INSTITUTION_ORGANIZATION_WHERE_CLIENT_RECEIVED_SERVICES = "type_of_institution_organization_where_client_received_services";
+const TYPE_OF_EVENT_ATTENDED = "type_of_event_attended";
+const TYPE_OF_SERVICE = "type_of_service";
+const MAIN_TOPIC_FOCUS_OF_THE_SERVICE_RECEIVED = "main_topic_focus_of_the_service_received";
+const SERVICE_RECEIVED = "service_received";
+const NUMBER_OF_UNIQUE_PARTICIPANTS = "number_of_unique_participants";
+const STATUS_OF_SERVICE = "status_of_service";
+const REASON_FOR_LEAVING_SERVICE = "reason_for_leaving_service";
+const CHILD_1_AGE = "child_1_age";
+const CHILD_1_TYPE_OF_CARE = "child_1_type_of_care";
+const CHILD_2_AGE = "child_2_age";
+const CHILD_2_TYPE_OF_CARE = "child_2_type_of_care";
+const CHILD_3_AGE = "child_3_age";
+const CHILD_3_TYPE_OF_CARE = "child_3_type_of_care";
+const CHILD_4_AGE = "child_4_age";
+const CHILD_4_TYPE_OF_CARE = "child_4_type_of_care";
+const CHILD_5_AGE = "child_5_age";
+const CHILD_5_TYPE_OF_CARE = "child_5_type_of_care";
+const BETWEEN_TRANSLATION = "between_translation";
+const AND_TRANSLATION = "and_translation";
+const BETWEEN_INTERPRETATION = "between_interpretation";
+const AND_INTERPRETATION = "and_interpretation";
+const TOTAL_LENGTH_OF_SERVICE_HOURS = "total_length_of_service_hours";
+const TOTAL_LENGTH_OF_SERVICE_MINUTES = "total_length_of_service_minutes";
+const REASON_FOR_UPDATE = "reason_for_update";
+const TOTAL_LENGTH_OF_ORIENTATION = "total_length_of_orientation";
+const TOTAL_LENGTH_OF_ORIENTATION_HOURS = "total_length_of_orientation_hours";
+const TOTAL_LENGTH_OF_ORIENTATION_MINUTES = "total_length_of_orientation_minutes";
+const NUMBER_OF_CLIENTS_IN_GROUP = "number_of_clients_in_group";
+//
+
 function mapTypes()
 {
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -157,6 +269,19 @@ function mapTypes()
 	setType(IN_PERSON_INSTRUCTION_PERCENT, FIELDTYPE_STRING);
 	setType(ONLINE_DISTANCE_INSTRUCTION_PERCENT, FIELDTYPE_STRING);
 	setType(TOTAL_NUMBER_OF_SPOTS_IN_COURSE, FIELDTYPE_STRING);
+	
+		//103
+	setType(PROCESSING_DETAILS, FIELDTYPE_STRING);
+	setType(UPDATE_RECORD_ID, FIELDTYPE_STRING);
+	setType(UNIQUE_IDENTIFIER, FIELDTYPE_STRING);
+	setType(UNIQUE_IDENTIFIER_VALUE, FIELDTYPE_STRING);
+	setType(DATE_OF_BIRTH_YYYY_MM_DD, FIELDTYPE_STRING);
+	setType(POSTAL_CODE_WHERE_THE_SERVICE_WAS_RECEIVED, FIELDTYPE_STRING);
+	setType(START_DATE_YYYY_MM_DD, FIELDTYPE_STRING);
+	setType(END_DATE_YYYY_MM_DD, FIELDTYPE_STRING);
+	setType(PROJECTED_END_DATE_YYYY_MM_DD, FIELDTYPE_STRING);
+	setType(START_DATE_OF_SERVICE_YYYY_MM_DD, FIELDTYPE_STRING);
+		//
 	
 	//Boolean fields go here
 	setType(HAS_EMAIL_ADDRESS, FIELDTYPE_BOOLEAN);
@@ -215,6 +340,69 @@ function mapTypes()
 	setType(REGISTRATION_IN_AN_EMPLOYMENT_INTERVENTION, FIELDTYPE_BOOLEAN);
 	setType(COURSE_HELD_ON_AN_ONGOING_BASIS, FIELDTYPE_BOOLEAN);
 	
+		//103
+	setType(DID_VOLUNTEERS_FROM_THE_HOST_COMMUNITY_PARTICIPATE_IN_THE_ACTIVITY, FIELDTYPE_BOOLEAN);
+	setType(DIRECTED_AT_A_SPECIFIC_TARGET_GROUP, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_CHILDREN_0_14_YRS, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_YOUTH_15_24_YRS, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_SENIOR, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_GENDER_SPECIFIC, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_REFUGEES, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_ETHNIC_CULTURAL_LINGUISTIC_GROUP, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_DEAF_OR_HARD_OF_HEARING, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_BLIND_OR_PARTIALLY_SIGHTED, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_LESBIAN_GAY_BISEXUAL_TRANSGENDER_QUEER_LGBTQ, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_FAMILIES_PARENTS, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_OTHER_IMPAIRMENTS_PHYSICAL_MENTAL, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_CLIENTS_WITH_INTERNATIONAL_TRAINING_IN_A_REGULATED_PROFESSION, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_CLIENTS_WITH_INTERNATIONAL_TRAINING_IN_A_REGULATED_TRADE, FIELDTYPE_BOOLEAN);
+	setType(TARGET_GROUP_OFFICIAL_LANGUAGE_MINORITIES, FIELDTYPE_BOOLEAN);
+	setType(WAS_ESSENTIAL_SKILLS_AND_APTITUDES_TRAINING_RECEIVED_AS_PART_OF_THE_SERVICE, FIELDTYPE_BOOLEAN);
+	setType(COMPUTER_SKILLS, FIELDTYPE_BOOLEAN);
+	setType(DOCUMENT_USE, FIELDTYPE_BOOLEAN);
+	setType(INTERPERSONAL_SKILLS_AND_WORKPLACE_CULTURE, FIELDTYPE_BOOLEAN);
+	setType(LEADERSHIP_TRAINING, FIELDTYPE_BOOLEAN);
+	setType(LIFE_SKILLS, FIELDTYPE_BOOLEAN);
+	setType(NUMERACY, FIELDTYPE_BOOLEAN);
+	setType(SUPPORT_SERVICES_RECEIVED, FIELDTYPE_BOOLEAN);
+	setType(CARE_FOR_NEWCOMER_CHILDREN, FIELDTYPE_BOOLEAN);
+	setType(TRANSPORTATION_CHILD, FIELDTYPE_BOOLEAN);
+	setType(PROVISIONS_FOR_DISABILITIES, FIELDTYPE_BOOLEAN);
+	setType(TRANSLATION, FIELDTYPE_BOOLEAN);
+	setType(INTERPRETATION, FIELDTYPE_BOOLEAN);
+	setType(CRISIS_COUNSELLING, FIELDTYPE_BOOLEAN);
+	setType(OVERVIEW_OF_CANADA, FIELDTYPE_BOOLEAN);
+	setType(OVERVIEW_OF_CANADA_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(SOURCES_OF_INFORMATION, FIELDTYPE_BOOLEAN);
+	setType(SOURCES_OF_INFORMATION_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(RIGHTS_AND_FREEDOMS, FIELDTYPE_BOOLEAN);
+	setType(RIGHTS_AND_FREEDOMS_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(CANADIAN_LAW_AND_JUSTICE, FIELDTYPE_BOOLEAN);
+	setType(CANADIAN_LAW_AND_JUSTICE_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(IMPORTANT_DOCUMENTS, FIELDTYPE_BOOLEAN);
+	setType(IMPORTANT_DOCUMENTS_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(IMPROVING_ENGLISH_OR_FRENCH, FIELDTYPE_BOOLEAN);
+	setType(IMPROVING_ENGLISH_OR_FRENCH_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(EMPLOYMENT_AND_INCOME, FIELDTYPE_BOOLEAN);
+	setType(EMPLOYMENT_AND_INCOME_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(EDUCATION, FIELDTYPE_BOOLEAN);
+	setType(EDUCATION_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(HOUSING, FIELDTYPE_BOOLEAN);
+	setType(HOUSING_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(HEALTH, FIELDTYPE_BOOLEAN);
+	setType(HEALTH_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(MONEY_AND_FINANCES, FIELDTYPE_BOOLEAN);
+	setType(MONEY_AND_FINANCES_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(TRANSPORTATION_ADULT, FIELDTYPE_BOOLEAN);
+	setType(TRANSPORTATION_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(COMMUNICATIONS_AND_MEDIA, FIELDTYPE_BOOLEAN);
+	setType(COMMUNICATIONS_AND_MEDIA_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(COMMUNITY_ENGAGEMENT, FIELDTYPE_BOOLEAN);
+	setType(COMMUNITY_ENGAGEMENT_REFERRALS, FIELDTYPE_BOOLEAN);
+	setType(BECOMING_A_CANADIAN_CITIZEN, FIELDTYPE_BOOLEAN);
+	setType(BECOMING_A_CANADIAN_CITIZEN_REFERRALS, FIELDTYPE_BOOLEAN);
+		//
+	
 	//Fields that have a specific list of choices go here (ones that have drop-down lists)
 	setType(UNIQUE_IDENTIFIER, FIELDTYPE_SELECT);
 	setType(STREET_TYPE, FIELDTYPE_SELECT);
@@ -248,6 +436,42 @@ function mapTypes()
 	setType(TIME_SPENT_WITH_CLIENT_ADDRESSING_CLIENTS_EMPLOYMENT_NEEDS_MINUTES, FIELDTYPE_SELECT);
 	setType(FORMAT_OF_TRAINING_PROVIDED, FIELDTYPE_SELECT);
 	setType(CLASSES_HELD_AT, FIELDTYPE_SELECT);
+	
+		//103
+	setType(LANGUAGE_OF_SERVICE, FIELDTYPE_SELECT);
+	setType(OFFICIAL_LANGUAGE_OF_PREFERENCE, FIELDTYPE_SELECT);
+	setType(REFERRED_BY, FIELDTYPE_SELECT);
+	setType(ACTIVITY_UNDER_WHICH_CLIENT_RECEIVED_SERVICES, FIELDTYPE_SELECT);
+	setType(TYPE_OF_INSTITUTION_ORGANIZATION_WHERE_CLIENT_RECEIVED_SERVICES, FIELDTYPE_SELECT);
+	setType(TYPE_OF_EVENT_ATTENDED, FIELDTYPE_SELECT);
+	setType(TYPE_OF_SERVICE, FIELDTYPE_SELECT);
+	setType(MAIN_TOPIC_FOCUS_OF_THE_SERVICE_RECEIVED, FIELDTYPE_SELECT);
+	setType(SERVICE_RECEIVED, FIELDTYPE_SELECT);
+	setType(NUMBER_OF_UNIQUE_PARTICIPANTS, FIELDTYPE_SELECT);
+	setType(STATUS_OF_SERVICE, FIELDTYPE_SELECT);
+	setType(REASON_FOR_LEAVING_SERVICE, FIELDTYPE_SELECT);
+	setType(CHILD_1_AGE, FIELDTYPE_SELECT);
+	setType(CHILD_1_TYPE_OF_CARE, FIELDTYPE_SELECT);
+	setType(CHILD_2_AGE, FIELDTYPE_SELECT);
+	setType(CHILD_2_TYPE_OF_CARE, FIELDTYPE_SELECT);
+	setType(CHILD_3_AGE, FIELDTYPE_SELECT);
+	setType(CHILD_3_TYPE_OF_CARE, FIELDTYPE_SELECT);
+	setType(CHILD_4_AGE, FIELDTYPE_SELECT);
+	setType(CHILD_4_TYPE_OF_CARE, FIELDTYPE_SELECT);
+	setType(CHILD_5_AGE, FIELDTYPE_SELECT);
+	setType(CHILD_5_TYPE_OF_CARE, FIELDTYPE_SELECT);
+	setType(BETWEEN_TRANSLATION, FIELDTYPE_SELECT);
+	setType(AND_TRANSLATION, FIELDTYPE_SELECT);
+	setType(BETWEEN_INTERPRETATION, FIELDTYPE_SELECT);
+	setType(AND_INTERPRETATION, FIELDTYPE_SELECT);
+	setType(TOTAL_LENGTH_OF_SERVICE_HOURS, FIELDTYPE_SELECT);
+	setType(TOTAL_LENGTH_OF_SERVICE_MINUTES, FIELDTYPE_SELECT);
+	setType(REASON_FOR_UPDATE, FIELDTYPE_SELECT);
+	setType(TOTAL_LENGTH_OF_ORIENTATION, FIELDTYPE_SELECT);
+	setType(TOTAL_LENGTH_OF_ORIENTATION_HOURS, FIELDTYPE_SELECT);
+	setType(TOTAL_LENGTH_OF_ORIENTATION_MINUTES, FIELDTYPE_SELECT);
+	setType(NUMBER_OF_CLIENTS_IN_GROUP, FIELDTYPE_SELECT);
+		//
 }
 
 function mapLists()

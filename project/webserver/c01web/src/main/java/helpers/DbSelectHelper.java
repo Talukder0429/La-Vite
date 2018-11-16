0263efee580a23a77cb2bc86af28d6aca87dff24
oopsidoodles <vilimilner@gmail.com>
2018-11-15 20:21:04 -0500

    added DbSelectHelper object, not very useful at the moment

---
+++ project/webserver/c01web/src/main/java/helpers/DbSelectHelper.java
@@ -0,0 +1,27 @@
+package helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import queryhelper.QueryParameter;
+
+public class DbSelectHelper
+{
+	private String tableName;
+	
+	private List<QueryParameter> resultFields;
+	private List<QueryParameter> conditionFields;
+	
+	public DbSelectHelper(String tableName)
+	{
+		this.tableName = tableName;
+		
+		this.resultFields = new ArrayList<QueryParameter>();
+		this.conditionFields = new ArrayList<QueryParameter>();
+	}
+	
+	public void addResultField(String field)
+	{
+		
+	}
+}

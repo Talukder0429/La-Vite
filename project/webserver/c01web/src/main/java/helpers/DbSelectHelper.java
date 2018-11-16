4818e78840f1c9a8f07d9ac594b735ad3d12e398
oopsidoodles <vilimilner@gmail.com>
2018-11-15 20:39:49 -0500

    added way to build select query strigns with no conditions or specific fields specified

--- project/webserver/c01web/src/main/java/helpers/DbSelectHelper.java
+++ project/webserver/c01web/src/main/java/helpers/DbSelectHelper.java
@@ -4,24 +4,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 import queryhelper.QueryParameter;
+import queryhelper.QueryParameterValue;
 
 public class DbSelectHelper
 {
 	private String tableName;
 	
 	private List<QueryParameter> resultFields;
-	private List<QueryParameter> conditionFields;
+	private List<QueryParameterValue> conditionFields;
 	
 	public DbSelectHelper(String tableName)
 	{
 		this.tableName = tableName;
 		
 		this.resultFields = new ArrayList<QueryParameter>();
-		this.conditionFields = new ArrayList<QueryParameter>();
+		this.conditionFields = new ArrayList<QueryParameterValue>();
 	}
 	
 	public void addResultField(String field)
 	{
 		QueryParameter qb = new QueryParameter(field);
+		this.resultFields.add(qb);
+	}
+	
+	public void addConditionField(String field, String value)
+	{
+		QueryParameterValue qbv = new QueryParameterValue(field, value);
+		this.conditionFields.add(qbv);
+	}
+	
+	private String buildQueryString()
+	{
+		String result = "";
+		
+		result += "SELECT ";
+		result += this.buildResultFieldsString();
+		result += " FROM ";
+		result += this.tableName + " ";
+		result += this.buildConditionFieldsString();
+		result += ";";
+		
+		System.out.println(result);
+		return result;
+	}
+	
+	private String buildResultFieldsString()
+	{
+		String result = "";
+		
+		if (this.resultFields.size() == 0)
+		{
+			result += "*";
+		}
+		
+		return result;
+	}
+	
+	private String buildConditionFieldsString()
+	{
+		String result = "";
+		
+		if (this.conditionFields.size() > 0)
+		{
+		}
+		
+		return result;
 	}
 }

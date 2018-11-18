4a8b89c226c564544147288e21b4516c84cbacc4
oopsidoodles <vilimilner@gmail.com>
2018-11-18 18:54:58 -0500

    added Calendar to date converter, and some functions to get the year and month, added boolean check to see if supplied date converter has the same year and month

--- project/webserver/c01web/src/main/java/helpers/DateConverter.java
+++ project/webserver/c01web/src/main/java/helpers/DateConverter.java
@@ -3,32 +3,70 @@ package helpers;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /** Converts strings into dates for certain fields
  * @author tonyz
  */
 public class DateConverter {
-	String date_string = "";
-	Date date;
+	private String date_string = "";
+	private Date date;
+	private Calendar calendar;
 	
 	public DateConverter(String text) throws ParseException {
-		date_string = text;
+		this.date_string = text;
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-		date = df.parse(text);
+		this.date = df.parse(text);
+		this.setCalendar();
 	}
 	
 	public DateConverter(Date input_date) {
-		date = input_date;
+		this.date = input_date;
 		DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
-		date_string = df.format(input_date);
+		this.date_string = df.format(input_date);
+		this.setCalendar();
 	}
+	
+	private void setCalendar()
+	{
+		this.calendar = Calendar.getInstance();
+		this.calendar.setTime(this.date);
+	}
+	
 	public String getDateAsString(){
-		return date_string;
+		return this.date_string;
 	}	
+	
 	public Date getDate() {
-		return date;
+		return this.date;
 	}
 	
+	public Calendar getCalendar()
+	{
+		return this.calendar;
+	}
 	
+	public int getYear()
+	{
+		return this.calendar.get(Calendar.YEAR);
+	}
+	
+	public int getMonth()
+	{
+		return this.calendar.get(Calendar.MONTH);
+	}
+	
+	//returns true if this converter and the one supplied have same year and date
+	public boolean isSameYearAndMonth(DateConverter dc)
+	{
+		if (this.getYear() == dc.getYear())
+		{
+			if (this.getMonth() == dc.getMonth())
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 }

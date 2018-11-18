096a806a2038043b959d2fbadeb0052cd4f37adb
Arnob <arnob_talukder@hotmail.com>
2018-11-17 22:13:43 -0500

    made uploader servlet

--- project/webserver/c01web/src/main/java/helpers/FileUploadHandler.java
+++ project/webserver/c01web/src/main/java/helpers/FileUploadHandler.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +15,7 @@ import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+@WebServlet("/FileUploadHandler")
 public class FileUploadHandler extends HttpServlet {
 	private static final long serialVersionUID = 1;
 
